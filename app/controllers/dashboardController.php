475d8a4ef307f3dd08bdb91d50a58c113944f5e4
Patrick Vienneau <pvienneau@carbure.co>
2014-06-19 16:41:00 -0400

    Generic Dashboard controller

---
+++ app/controllers/dashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+class dashboardController extends Controller
+{
+	public function __construct(){		
+		if ( ! AuthUser::isLoggedIn()) {
+			redirect(get_url('login'));
+		}
+		
+		$this->setLayout('user');
+	}
+	
+	public function index(){
+		echo 'welcome';
+	}
+
+} // end UserController class
