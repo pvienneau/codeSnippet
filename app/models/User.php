d5fc952dd0037145e7c80dfb5813e2b22b8e298a
Patrick Vienneau <pvienneau@carbure.co>
2014-06-19 15:34:56 -0400

    user model start

--- app/models/User.php
+++ app/models/User.php
@@ -12,7 +12,9 @@ class User extends Record
 			$password = false,
 			$date_created = false,
 			$date_last_login = false;
-			
 	
+	public static function findByUsername($username){
+		return self::findOneFrom(self::TABLE_NAME, 'username=?', array($username));
+	}
 	
 } // end User class
