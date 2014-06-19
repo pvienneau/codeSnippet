4ed4f4c1d05157f0ea68967df4925246c9601277
Patrick Vienneau <pvienneau@carbure.co>
2014-06-19 16:41:39 -0400

    Clean bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-//define('DEFAULT_RATE', 50);
-//define('DEFAULT_CONTROLLER', 'invoice');
-
 define('CORE_ROOT', dirname(__FILE__));
 define('APP_PATH', CORE_ROOT.DIRECTORY_SEPARATOR.'app');
 
@@ -35,7 +32,7 @@ Dispatcher::addRoute(array(
 	'/login' => 'authentication/login',
 	'/logout' => 'authentication/logout',
 	'/profile/:any' => 'user/view/$1',
-	'/profile' => 'user/view/'
+	'/profile' => 'user/profile'
 ));
 
 Dispatcher::dispatch(isset($_GET['u']) ? $_GET['u']: AuthUser::getDefaultController());
