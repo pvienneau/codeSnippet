d6cbaf5a3f2494f5c7349815156499ce0eab2197
Patrick Vienneau <pvienneau@carbure.co>
2014-06-20 10:15:17 -0400

    use get_url to get path to correct route in HTML

--- app/views/authentication/login.php
+++ app/views/authentication/login.php
@@ -1,4 +1,4 @@
-<form action="login" method="POST">
+<form action="<?php echo get_url('login'); ?>" method="POST">
 	<input type="text" name="username" /><br/>
 	<input type="password" name="password" /><br/>
 	<button type="submit" name="action_user_login">Save</button>
