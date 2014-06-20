05a65987dfdb1c7017969e254a3ad3fde299d251
Patrick Vienneau <pvienneau@carbure.co>
2014-06-20 11:28:04 -0400

    new code model

--- app/models/Code.php
+++ app/models/Code.php
@@ -9,5 +9,28 @@ class Code extends Record
 			$title = false,
 			$deleted = 0,
 			$date_created = false;
+			
+	// public function __construct(){
+	// 		parent::__construct();
+	// 		
+	// 		$this->date_created = mysql_now();
+	// 	}
+		
+	public function beforeInsert(){
+		if(!self::_validate_data()) return FALSE;
+		
+		$this->date_created = mysql_now();
+		
+		return TRUE;
+	}
+			
+	private function _validate_data(){
+		if(
+			empty($this->description) ||
+			empty($this->title)
+		) return false;
+		
+		return true;
+	}
 	
 } // end Code class
